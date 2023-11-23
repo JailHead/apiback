2ad05715411ba6a1b2a6e61be2a9bb5d5193fbee
JailHead <144750822+JailHead@users.noreply.github.com>
2023-11-23 16:47:00 -0600

    Complete the project about node server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 // const app = createServer((request, respones) => {
 //   Response.writeHead(200, {'Content-Type':'aplication/json'});
@@ -45,23 +46,36 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.paramas.id)
+  const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
 
 app.post('/api/notes', (request, response) => {
   const note = request.body;
+
+  if (!note||!note.content) {
+    return response.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
   const ids = notes.map(note => note.id)
   const maxId = Math.max(...ids)
+
   const newNote = {
     id: maxId +1,
     content: note.content,
-    import: typeof note.important != 'undefined' ? note.important:false,
+    important: typeof note.important !== 'undefined' ? note.important:false,
     date: new Date().toISOString()
   }
-  note = notes.concat(newNote)
-  response.json(newNote)
+  notes = [...notes, newNote]
+  response.status(201).json(newNote)
+})
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
 })
 
 const PORT = 4000;
